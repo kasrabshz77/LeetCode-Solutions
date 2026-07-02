f62d03133360bffe4dd19d5a75cf99187a21d9df
Kasra <bashizadehkasra@gmail.com>
2026-07-02 14:58:09 -0400

    Update two_sum.py

--- array-string/two_sum.py
+++ array-string/two_sum.py
@@ -1,1 +1,12 @@
-
+#Time complexity: O(n)
+class Solution(object):
+    def twoSum(self, nums, target):
+        mp = {}
+        for i in range(len(nums)):
+            mp[nums[i]] = i 
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in mp and mp[need] != i:
+                return [i, mp[need]]
+        return -1
+     
