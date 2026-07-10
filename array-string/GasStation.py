7c053ecc4cedb340f5994d604d957a8a4adf50c2
Kasra <bashizadehkasra@gmail.com>
2026-07-10 14:02:21 -0400

    Create GasStation.py

---
+++ array-string/GasStation.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        total = 0 
+        tank = 0 
+        start = 0 
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            total+=diff
+            tank +=diff
+            if tank < 0:
+                start = i+1
+                tank = 0 
+        if total < 0:
+            return -1 
+
+        return start 
+        
