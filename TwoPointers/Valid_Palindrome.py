b35b108912a77d35a2d129a98908f363572c0897
Kasra <bashizadehkasra@gmail.com>
2026-07-10 14:53:18 -0400

    Update Valid_Palindrome.py

--- TwoPointers/Valid_Palindrome.py
+++ TwoPointers/Valid_Palindrome.py
@@ -1,1 +1,7 @@
-
+class Solution(object):
+    def isPalindrome(self, s):
+        string = ""
+        for i in s:
+            if i.isalnum():
+                string+=i.lower()
+        return string == string[::-1]
