a3a5d06d4520c89748b599420abf570c6b0c2e4c
Kasra <bashizadehkasra@gmail.com>
2026-07-22 15:19:58 -0400

    Implement twoSum function in two_sum.py

---
+++ Hashmap/two_sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        mp = {}
+        for i in range(len(nums)):
+            mp[nums[i]] = i 
+        
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in mp and mp[need] != i:
+                return [mp[need], i]
+        
