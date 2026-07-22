0d3942fea811c68dd0eea36908fb0cdfc1a47856
Kasra <bashizadehkasra@gmail.com>
2026-07-22 15:17:26 -0400

    Create group_anagrams.py

---
+++ Hashmap/group_anagrams.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            if key in groups:
+                groups[key].append(word)
+            else:
+                groups[key] = [word]
+        return list(groups.values())
+        
